7b6ae0f645bba06e22c720560a26ff718cb29ee3
Srithwak <60199432+Srithwak@users.noreply.github.com>
2023-10-17 12:52:28 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,45 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path');
+const fs = require('fs'); //json
+
+// const isDev = process.env.NODE_ENV !== 'development'; //check if admin or not
+const isDev = false;
+const isMac = process.platform === 'darwin'; //checks if app is running on mac
+
+function createWindow(htmlFile = 'login.html', preloadFile = "loginPreload.js") {
+   // Create the browser window.
+   const win = new BrowserWindow({
+      title: 'Title',
+      width: isDev ? 976 : 655,
+      height: 600,
+      minHeight: 655,
+      minWidth: 600,
+      icon: "", //this doesn't work obv
+      webPreferences: {
+         nodeIntegration: true,
+         contextIsolation: false,
+         preload: `${__dirname}/${preloadFile}`
+      }
+   });
+   if (isDev) {
+      win.openDevTools();
+   }
+   // load the htmlFile of the app.
+   win.loadFile(htmlFile)
+}
+
+app.whenReady().then(() => {
+   createWindow();
+
+   app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0)
+         createWindow();
+   });
+});
+
+//closes app on mac
+app.on('window-all-closed', () => {
+   if (!isMac) {
+      app.quit();
+   }
+});
