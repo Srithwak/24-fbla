d24412429bc3f4134d978fcad1bf3472ccb1b71b
Srithwak <60199432+Srithwak@users.noreply.github.com>
2023-10-18 11:11:27 -0400

    Update signUp.js

--- src/assets/signUp.js
+++ src/assets/signUp.js
@@ -1,1 +1,36 @@
-/
+//Has not been tested
+//line 20, 22 not complete
+const {fs, getJSON, uploadJSON, sortByID, findNextID, makePartnerAccount, makeSchoolAccount, errorPopup} = require('./common.js');
+
+function toRun(){
+  const username = document.querySelector(".username").value;
+  const password1 = document.querySelector(".password1").value;
+  const password2 = document.querySelector(".password2").value;
+  const email = document.querySelector(".email").value;
+  if(searchAttribute('username').includes(username)){
+    console.log("Username already exists"); //errorPopup
+    return false;
+  } else if(searchAttribute('email').includes(email)){
+    console.log("Email already exists"); //errorPopup
+    return false;
+  } 
+  else if(password1 != password2){
+    console.log("Passwords do not match"); //errorPopup
+    return false;
+  } //else if- password restrictions
+  let partner = false, school = false;
+  //include an option to select partner or school account, it should just toggle partner or school, it should also make sure to check the html that only one can be selected at a time, radio buttons or sth
+  if(document.querySelector(".partner").checked)
+    partner = true;
+  else if(document.querySelector(".school").checked)
+    school = true;
+  else {
+    console.log("No account type selected"); //errorPopup
+    return false;
+  }
+  if(partner)
+    makePartnerAccount(username, password, email);
+  else if(school)
+    makeSchoolAccount(username, password, email);
+  location.href = 'login.html';
+}
