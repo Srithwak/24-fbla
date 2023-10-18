8e298a23fbc5faf668cbab802b8480f956652f3e
Srithwak <60199432+Srithwak@users.noreply.github.com>
2023-10-18 10:11:21 -0400

    Update helper.js

--- src/assets/helper/helper.js
+++ src/assets/helper/helper.js
@@ -1,48 +1,68 @@
-const fs = require('fs');
+const fs = require('fs'); //json
 
-const readJSON = file => JSON.parse(fs.readFileSync(file));
-// function overwrites any perviously existing data so data must be read first and then any updates can be pushed
-const writeJSON = (file, data) => {
-   fs.writeFileSync(file, JSON.stringify(data, null, 2), { encoding: 'utf8', flag: 'w' });
-   console.log('function writeJSON(): write completed successfully');
+function getJSON(file) { //gets data from JSON file in a useable format
+    return JSON.parse(fs.readFileSync(file));
 }
-const sortByID = file => readJSON(file).sort((a, b) => a.id - b.id);
-const findNextID = file => {
-   let data = sortByID(file);
-   for (let i = 0; i < data.length; i++) if (data[i].id !== i) return i;
-   return data.length;
-}
-// const partner = (
-//    f_name,
-//    l_name,
-//    description,
-//    balance,
-//    resources,
-//    email,
-//    phone1,
-//    phone2,
-//    website,
-//    social_media
-// ) => {}
 
-function makePartner(name, description, money, resources, email, phone1, phone2, website, socialMedia) {
-   let partner = {
-      id: findNextID("partners.json"),
-      name: name,
-      description: description,
-      money: money,
-      resources: resources,
-      email: email,
-      phone1: phone1,
-      phone2: phone2,
-      website: website,
-      socialMedia: socialMedia
-   }
-   uploadJSON("partners.json", partner);
-   return partner;
+function uploadJSON(file, data) { //overwrites JSON file and uploads with data
+    fs.writeFileSync(file, JSON.stringify(data, null, 2), {
+        encoding: 'utf8',
+        flag: 'w'
+    });
+    console.log("Upload complete");
+}
+
+function sortByID(jsonFile) { //sorts the json file based on id
+  return getJSON(jsonFile).sort((a, b) => a.id - b.id);
+}
+
+function findNextID(jsonFile) { //finds the next available ID to use
+  let jsonData = sortByID(jsonFile);
+  for(let i = 0; i < jsonData.length; i++)
+    if(jsonData[i].id != i)
+      return i;
+  return jsonData.length;
+}
+
+function makePartner(name, description, email){ //not tested
+  let account = {
+    id: findNextID("accounts.json"),
+    username: name,
+    password: null,
+    description: description,
+    money: 0,
+    resources: [],
+    email: email,
+    phone: 0,
+    website: "",
+    socialMedia: []
+  }
+  uploadJSON("accounts.json", getJSON('accounts.json').push(account));
+  return account;
+}
+
+function makeSchoolAccount(username, password, email) { //not tested
+  let account = {
+    id: findNextID("accounts.json"),
+    username: username,
+    password: password,
+    email: email,
+    phone: null,
+    website: null,
+    messages: [],
+    socialMedia: [],
+    notes: [],
+    hashKey: ""
+  }
+  uploadJSON("accounts.json", getJSON('accounts.json').push(account));
+  return account;
 }
 
 function errorPopup() { //error popup function
+  
 }
 
-module.exports = { getJSON, uploadJSON, sortByID, findNextID, makePartner, errorPopup };
+module.exports = {
+  fs, getJSON, uploadJSON, sortByID, findNextID, makePartnerAccount, makeSchoolAccount, errorPopup
+};
+
