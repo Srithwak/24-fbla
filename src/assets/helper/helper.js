ab3ac3b264ddcdcd93d89d88b05fd30ab425bb1b
PraneethRangarajan <prangarajan.git@gmail.com>
2023-10-19 12:45:34 -0400

    bugfixes in signup

--- src/assets/helper/helper.js
+++ src/assets/helper/helper.js
@@ -1,80 +1,85 @@
 const fs = require('fs'); //json
+const path = require('path')
 
 function getJSON(file) { //gets data from JSON file in a useable format
-    return JSON.parse(fs.readFileSync(file));
+   return JSON.parse(fs.readFileSync(file));
 }
 
 function uploadJSON(file, data) { //overwrites JSON file and uploads with data
-    fs.writeFileSync(file, JSON.stringify(data, null, 2), {
-        encoding: 'utf8',
-        flag: 'w'
-    });
-    console.log("Upload complete");
+   fs.writeFileSync(file, JSON.stringify(data, null, 2), {
+      encoding: 'utf8',
+      flag: 'w'
+   });
+   console.log("Upload complete");
 }
 
-function sortByID(jsonFile = (__dirname, '../database/accounts.json')) { //sorts the json file based on id
-  return getJSON(jsonFile).sort((a, b) => a.id - b.id);
+function sortByID(jsonFile = (path.join(__dirname, '../../database/accounts.json'))) { //sorts the json file based on id
+   return getJSON(jsonFile).sort((a, b) => a.id - b.id);
 }
 
-function findNextID(jsonFile = (__dirname, '../database/accounts.json')) { //finds the next available ID to use
-  let jsonData = sortByID(jsonFile);
-  for(let i = 0; i < jsonData.length; i++)
-    if(jsonData[i].id != i)
-      return i;
-  return jsonData.length;
+function findNextID(jsonFile = (path.join(__dirname, '../../database/accounts.json'))) { //finds the next available ID to use
+   let jsonData = sortByID(jsonFile);
+   for (let i = 0; i < jsonData.length; i++)
+      if (jsonData[i].id != i)
+         return i;
+   return jsonData.length;
 }
 
-function makePartnerAccount(username, password, description, email){ //not tested
-  let account = {
-    id: findNextID(__dirname, '../database/accounts.json'),
-    username: username,
-    password: password,
-    description: null,
-    money: 0,
-    resources: [],
-    email: email,
-    phone: 0,
-    website: "",
-    socialMedia: [],
-    messages: [],
-    associated: [],
-    hashKey: ""
-  }
-  uploadJSON(__dirname, '../database/accounts.json', getJSON(__dirname, '../database/accounts.json').push(account));
-  return account;
+function makePartnerAccount(username, password, email) { //not tested
+   let account = {
+      id: findNextID(path.join(__dirname, '../../database/accounts.json')),
+      username: username,
+      password: password,
+      description: null,
+      money: 0,
+      resources: [],
+      email: email,
+      phone: 0,
+      website: "",
+      socialMedia: [],
+      messages: [],
+      associated: [],
+      hashKey: ""
+   }
+   // uploadJSON(path.join(__dirname, '../../database/accounts.json'), getJSON(path.join(__dirname, '../../database/accounts.json')).push(account));
+   console.log(account);
+   return account;
 }
 
 function makeSchoolAccount(username, password, email) { //not tested
-  let account = {
-    id: findNextID(__dirname, '../database/accounts.json'),
-    username: username,
-    password: password,
-    email: email,
-    phone: null,
-    website: null,
-    messages: [],
-    socialMedia: [],
-    associated: [],
-    notes: [],
-    hashKey: ""
-  }
-  uploadJSON(__dirname, '../database/accounts.json', getJSON(__dirname, '../database/accounts.json').push(account));
-  return account;
+   let account = {
+      id: findNextID(path.join(__dirname, '../../database/accounts.json')),
+      username: username,
+      password: password,
+      email: email,
+      phone: null,
+      website: null,
+      messages: [],
+      socialMedia: [],
+      associated: [],
+      notes: [],
+      hashKey: ""
+   }
+   // uploadJSON(path.join(__dirname, '../../database/accounts.json'), getJSON(path.join(__dirname, '../../database/accounts.json')).push(account));
+   console.log(account);
+   return account;
 }
 
-function searchAttribute(attribute, data = getJSON(__dirname, '../database/accounts.json')) {
-  return data.filter(obj => obj.hasOwnProperty(attribute)).map(obj => obj[attribute]);
+function searchAttribute(attribute, data) {
+   if (data === undefined) data = getJSON(path.join(__dirname, '../../database/accounts.json'))
+   return data.filter(obj => obj.hasOwnProperty(attribute)).map(obj => obj[attribute]);
 }
+// console.log(searchAttribute('username'));
 
-function getObj(id){
-  return getJSON(__dirname, '../database/accounts.json').find(obj => obj.id == id);
+function getObj(id) {
+   return getJSON(path.join(__dirname, '../../database/accounts.json')).find(obj => obj.id == id);
 }
 
 function errorPopup() { //error popup function
-  
+
 }
 
 module.exports = {
-  fs, getJSON, uploadJSON, sortByID, findNextID, makePartnerAccount, makeSchoolAccount, searchAttribute, errorPopup
+   fs, getJSON, uploadJSON, sortByID, findNextID, makePartnerAccount, makeSchoolAccount, searchAttribute, errorPopup
 };
 
